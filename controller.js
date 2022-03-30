9ab82236c9255faef1f7c10d7a6022ca9fd98be1
Anna Uyen K.P <79038087+a-ukp@users.noreply.github.com>
2022-03-30 00:21:58 -0500

    added icons

--- controller.js
+++ controller.js
@@ -7,11 +7,14 @@ const locationType = document.querySelector('.location');
 const rarity = document.querySelector('.rarity');
 const price = document.querySelector('.price');
 const museumPhrase = document.querySelector('.museum-phrase');
+const bugIcon = document.querySelector('.icon');
 
 button.addEventListener('click', function(){
   fetch('https://acnhapi.com/v1/bugs/'+input.value)
     .then(response => response.json())
     .then(data => {
+      console.log(data)
+
       let nameValue = data['name']['name-USen'];
       let monthAvailability = data['availability']['month-northern'];
       let timeAvailability = data['availability']['time'];
@@ -19,14 +22,16 @@ button.addEventListener('click', function(){
       let rarityValue = data['availability']['rarity'];
       let priceValue = data['price'];
       let blathersPhrase = data['museum-phrase'];
-
+      let iconValue = data['id'];
+      
       bugValue.innerHTML = "Name: " + nameValue;
       month.innerHTML = "Month Available: " + monthAvailability;
       time.innerHTML = "Time Available: " + timeAvailability;
       locationType.innerHTML = "Location: " + locationValue;
       rarity.innerHTML = "Rarity: " + rarityValue;
-      price.innerHTML = "Price: " + priceValue;
+      price.innerHTML = "Price: " + priceValue + " Bells";
       museumPhrase.innerHTML = "Blathers Fun Fact: " + blathersPhrase;
+      bugIcon.setAttribute("src", "https://acnhapi.com/v1/icons/bugs/" + iconValue);
     })
 
     .catch(err => alert("Sorry! We don't know that bug."))
