859812150dfc11a35e13738c3258b162f8f87262
Anna Uyen K.P <79038087+a-ukp@users.noreply.github.com>
2022-03-29 23:44:53 -0500

    switched to bugs API/finished JS

--- controller.js
+++ controller.js
@@ -1,30 +1,34 @@
-/* 
-  Created By: Anna K. Phung
-  Last Updated: March 29, 2022
-  Description: Fetch API call & DOM manipulation
-*/
+const input = document.querySelector('.inputValue');
+const button = document.querySelector('.submit');
+const bugValue = document.querySelector('.bug-name');
+const month = document.querySelector('.month-available');
+const time = document.querySelector('.time-available');
+const locationType = document.querySelector('.location');
+const rarity = document.querySelector('.rarity');
+const price = document.querySelector('.price');
+const museumPhrase = document.querySelector('.museum-phrase');
 
-async function getVillager(villagerName) {
-  villagerName = apiName(villagerName);
+button.addEventListener('click', function(){
+  fetch('https://acnhapi.com/v1/bugs/'+input.value)
+    .then(response => response.json())
+    .then(data => {
+      let nameValue = data['name']['name-USen'];
+      let monthAvailability = data['availability']['month-northern'];
+      let timeAvailability = data['availability']['time'];
+      let locationValue = data['availability']['location'];
+      let rarityValue = data['availability']['rarity'];
+      let priceValue = data['price'];
+      let blathersPhrase = data['museum-phrase'];
 
-  const req = new Request('https://acnhapi.com/v1/villagers/' + villagerName);
+      bugValue.innerHTML = "Name: " + nameValue;
+      month.innerHTML = "Month Available: " + monthAvailability;
+      time.innerHTML = timeAvailability;
+      locationType.innerHTML = locationValue;
+      rarity.innerHTML = rarityValue;
+      price.innerHTML = priceValue;
+      museumPhrase.innerHTML = blathersPhrase;
+    })
 
-  const response = await fetch(req);
-  if (!response.ok) {
-    throw new Error(res);
-  }
-
-  return response.json();
-};
-
-window.onload = () => {
-  document.querySelector("#srch-btn").addEventListener("click", updateVillager);
-  document.querySelector(".input-box").addEventListener("keydown", updateVillager);
-
-  let input = document.querySelector(".input-box");
-  input.focus();
-}
-
-let villagerData;
-
+    .catch(err => alert("Sorry! We don't know that bug."))
+})
 
