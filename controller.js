1e3dc42f98f69c939f4a72d154c93bb022519aff
unknown <grantleewelton@gmail.com>
2022-03-29 21:23:29 -0600

    final commit

---
+++ controller.js
@@ -0,0 +1,15 @@
+/*
+    Created by: Grant Welton
+    Creation Date: 25 Mar 2022
+    Last Updated: 29 Mar 2022
+    Description: Code to give interaction to the PokéFinder site
+*/
+
+let userInput = document.getElementById("user-input");
+
+button.addEventListener(onclick,);
+
+async function getPlayer(pokemonName) {
+    const request = new Request('https://pokeapi.co/api/v2/pokemon/'+playerName);
+}
+
